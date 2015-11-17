f9812050586a365ffe51cc85dfc49b0cd0e85945
Jim Baker <jim.baker@rackspace.com>
2015-11-17 12:58:04 -0700

    Minor cleanup

--- command.js
+++ command.js
@@ -7,24 +7,20 @@ const esprima = require('esprima'),
       process = require('process'),
       jsonjs = require('./lib')
 
-console.log(jsonjs)
-
 function main() {
   // FIXME add help!
-  console.log('Invoking jsonjs')
 
   // NOTE it's argv[2] because of the shebang invocation, namely
   // /usr/bin/env node FILENAME
   const filename = process.argv[2]
-  console.log("Parsing", filename)
-  const code = fs.readFileSync(filename, 'utf8')
+  console.log('Parsing program:', filename)
+  const program = fs.readFileSync(filename, 'utf8')
 
-  const env = Object.create(jsonjs.BindingEnvironment)
-  const expr = esprima.parse(code).body[0]  // FIXME be able to eval sequence of statements!
-  console.log('Evaluating', JSON.stringify(expr), null, '  ')
+  const env = Object.create(jsonjs.Bindings)
+  const expr = esprima.parse(program).body[0]  // FIXME be able to eval sequence of statements!
+  console.log('Evaluating:', JSON.stringify(expr), null, '  ')
 
-  console.log(jsonjs.myeval(env, expr))
+  console.log('Result:', jsonjs.myeval(env, expr))
 }
 
 main()
-
