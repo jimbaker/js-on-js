c1f5cae017376b367e9cc328427834177786521a
Jim Baker <jim.baker@rackspace.com>
2015-11-17 12:51:36 -0700

    Cleanup

--- command.js
+++ command.js
@@ -1,28 +1,29 @@
 #!/usr/bin/env node
 
-const fs = require('fs')
-const process = require('process')
-const jsonjs = require('js-on-js')
+'use strict'
 
+const esprima = require('esprima'),
+      fs = require('fs'),
+      process = require('process'),
+      jsonjs = require('./lib')
 
-function xmain() {
+console.log(jsonjs)
+
+function main() {
   // FIXME add help!
   console.log('Invoking jsonjs')
 
-  // NOTE it's argv[2] because invocation is /usr/bin/env node FILENAME
+  // NOTE it's argv[2] because of the shebang invocation, namely
+  // /usr/bin/env node FILENAME
   const filename = process.argv[2]
   console.log("Parsing", filename)
   const code = fs.readFileSync(filename, 'utf8')
 
   const env = Object.create(jsonjs.BindingEnvironment)
-  const expr = esprima.parse(code).body[0]  // be able to eval sequence of statements!
+  const expr = esprima.parse(code).body[0]  // FIXME be able to eval sequence of statements!
   console.log('Evaluating', JSON.stringify(expr), null, '  ')
 
   console.log(jsonjs.myeval(env, expr))
-}
-
-function main() {
-    console.log('logging version of jsonjs')
 }
 
 main()
