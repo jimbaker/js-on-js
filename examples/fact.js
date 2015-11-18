9c5c078764e49a1384ab97adc710aa6fc0091c01
Jim Baker <jim.baker@rackspace.com>
2015-11-18 16:19:14 -0700

    node note

--- examples/fact.js
+++ examples/fact.js
@@ -2,5 +2,9 @@ const fact = function(x) {
   if (x === 0) { return 1 }
   else { return x * fact(x - 1) }
 }
+
+// NOTE: for real JS compliance - at least with Node's semantics - we
+// need to do `console.log(fact(6))`
+
 fact(6)
 
