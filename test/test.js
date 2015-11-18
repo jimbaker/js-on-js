3fdcd15964ccc0d500224c21e3a678dac6c78ab9
Jim Baker <jim.baker@rackspace.com>
2015-11-18 08:46:37 -0700

    Refactor by reworking comments, better follow JS naming convention

--- test/test.js
+++ test/test.js
@@ -2,7 +2,7 @@ const assert = require('chai').assert,
       esprima = require('esprima'),
       jsonjs = require('../lib')
 
-function parse_expr(text, logExpr) {
+function parseExpr(text, logExpr) {
   var expr = esprima.parse(text)
   if (logExpr) {
     console.log("expr", JSON.stringify(expr, null, '  '))
@@ -12,84 +12,86 @@ function parse_expr(text, logExpr) {
 
 describe('nonexistence', function () {
   it('an empty program is undefined', function () {
-    assert.equal(jsonjs.eval(parse_expr('')), undefined)
+    assert.equal(jsonjs.eval(parseExpr('')), undefined)
   }),
   it('an empty statement is undefined', function () {
-    assert.equal(jsonjs.eval(parse_expr(';')), undefined)
+    assert.equal(jsonjs.eval(parseExpr(';')), undefined)
   }),
   it('an empty block is undefined', function () {
-    assert.equal(jsonjs.eval(parse_expr('{}')), undefined)
+    assert.equal(jsonjs.eval(parseExpr('{}')), undefined)
   })
 })
 
 describe('literals', function () {
-  it('a numeric literal should return itself', function () {
-    assert.equal(jsonjs.eval(parse_expr('123')), 123)
+  it('a numeric literal returns itself', function () {
+    assert.equal(jsonjs.eval(parseExpr('123')), 123)
   })
 })
 
 describe('binary expressions', function () {
-  it('multiplication should work on numbers', function () {
-    assert.equal(jsonjs.eval(parse_expr('6 * 9')), 54)
+  it('multiplication on numbers', function () {
+    assert.equal(jsonjs.eval(parseExpr('6 * 9')), 54)
   }),
-  it('addition should work on numbers', function () {
-    assert.equal(jsonjs.eval(parse_expr('11 + 22')), 33)
+  it('addition on numbers', function () {
+    assert.equal(jsonjs.eval(parseExpr('11 + 22')), 33)
   }),
-  it('subtraction should work on numbers', function () {
-    assert.equal(jsonjs.eval(parse_expr('25 - 16')), 9)
+  it('subtraction on numbers', function () {
+    assert.equal(jsonjs.eval(parseExpr('25 - 16')), 9)
   }),
-  it('strict equality should work on numbers, when equal', function () {
-    assert.equal(jsonjs.eval(parse_expr('47 === 47')), true)
+  it('strict equality on numbers, when equal', function () {
+    assert.equal(jsonjs.eval(parseExpr('47 === 47')), true)
   }),
-  it('strict equality should work on numbers, when unequal', function () {
-    assert.equal(jsonjs.eval(parse_expr('47 === 42')), false)
+  it('strict equality on numbers, when unequal', function () {
+    assert.equal(jsonjs.eval(parseExpr('47 === 42')), false)
   })
   // FIXME etc
 })
 
 describe('sequence of expressions', function () {
-  it('sequence of expressions should return the last value', function () {
-    assert.equal(jsonjs.eval(parse_expr('1,1,2,3,5,8')), 8)
+  it('sequence of expressions returns the last value', function () {
+    assert.equal(jsonjs.eval(parseExpr('1,1,2,3,5,8')), 8)
   })
 })
 
 describe('bindings', function () {
-  it('const assignment should create a binding', function () {
-    assert.equal(jsonjs.eval(parse_expr('const x = 47; x')), 47)
+  it('const assignment creates a binding', function () {
+    assert.equal(jsonjs.eval(parseExpr('const x = 47; x')), 47)
   }),
   it('const assignment results in undefined', function () {
-    assert.equal(jsonjs.eval(parse_expr('const x = 47')), undefined)
+    assert.equal(jsonjs.eval(parseExpr('const x = 47')), undefined)
   }),
-  it('unbound name should throw a ReferenceError', function () {
+  it('unbound names throw a ReferenceError', function () {
     assert.throws(
-      function () { jsonjs.eval(parse_expr('x')) }, ReferenceError)
+      function () { jsonjs.eval(parseExpr('x')) },
+      ReferenceError)
   }),
   it('const assignments are not visible outside a block scope', function () {
     assert.throws(
-      function () { jsonjs.eval(parse_expr('{ const x = 47 }; x')) }, ReferenceError)
+      function () { jsonjs.eval(parseExpr('{ const x = 47 }; x')) },
+      ReferenceError)
   })
 })
 
 describe('functions', function () {
-  it('function definition should create a binding', function () {
-    const func = jsonjs.eval(parse_expr('const x = function () {}; x'))
+  it('function definition creates a binding', function () {
+    const func = jsonjs.eval(parseExpr('const x = function () {}; x'))
     assert.equal(func.type, 'FunctionExpression')
     assert.equal(func.body.body.length, 0)  // empty body for this function
   }),
-  it('calling an empty function should return undefined', function () {
-    assert.equal(jsonjs.eval(parse_expr('const x = function () {}; x()')), undefined)
+  it('calling an empty function returns undefined', function () {
+    assert.equal(jsonjs.eval(parseExpr('const x = function () {}; x()')), undefined)
   }),
   it('calling a no-arg function', function () {
-    assert.equal(jsonjs.eval(parse_expr('const x = function () { return 47 }; x()')), 47)
+    assert.equal(jsonjs.eval(parseExpr('const x = function () { return 47 }; x()')), 47)
   }),
   it('calling a single arg function', function () {
-    assert.equal(jsonjs.eval(parse_expr('const add1 = function(x) { return x + 1 }; add1(41)')), 42)
+    assert.equal(jsonjs.eval(parseExpr('const add1 = function(x) { return x + 1 }; add1(41)')), 42)
   }),
   it('calling a single arg function immediately', function () {
-    assert.equal(jsonjs.eval(parse_expr('(function(x) { return x + 1 })(41)')), 42)
+    assert.equal(jsonjs.eval(parseExpr('(function(x) { return x + 1 })(41)')), 42)
   }),
   it('recursive functions work', function () {
-    const expr = parse_expr(
+    const expr = parseExpr(
       'const fact = function(x) {' +
       '  if (x === 0) { return 1 }' +
       '  else { return x * fact(x - 1) }' +
@@ -97,34 +99,35 @@ describe('functions', function () {
       'fact(6)')
     assert.equal(jsonjs.eval(expr), 720)
   })  
-  // FIXME obvious extensions to apply "alpha renaming" and get lexical scope
 })
 
 describe('conditional expressions and if statements', function () {
   it('true conditional only specifies consequent', function ()  {
-    assert.equal(jsonjs.eval(parse_expr('if (true) { 47 }')), 47)
+    assert.equal(jsonjs.eval(parseExpr('if (true) { 47 }')), 47)
   }),
   it('false conditional only specifies consequent returns undefined', function ()  {
-    assert.equal(jsonjs.eval(parse_expr('if (false) { 47 }')), undefined)
+    assert.equal(jsonjs.eval(parseExpr('if (false) { 47 }')), undefined)
   }),
   it('false conditional short circuit on consequent', function ()  {
     // ReferenceError would be raised if no short circuiting
-    assert.equal(jsonjs.eval(parse_expr('false ? x : 47')), 47)
+    assert.equal(jsonjs.eval(parseExpr('false ? x : 47')), 47)
   }),
   it('true conditional short circuit on alternate', function ()  {
     // ReferenceError would be raised if no short circuiting
-    assert.equal(jsonjs.eval(parse_expr('true ? 47 : x')), 47)
+    assert.equal(jsonjs.eval(parseExpr('true ? 47 : x')), 47)
   })
 })
 
 describe('unknown expressions', function () {
-  it('unknown expression type should raise an error', function ()  {
+  it('unsupported expression types raise an interpreter error', function ()  {
     assert.throws(
-      function () { jsonjs.eval(parse_expr('class Shape {}')) }, jsonjs.InterpreterError)
+      function () { jsonjs.eval(parseExpr('class Shape {}')) },
+      jsonjs.InterpreterError)
   }),
-  it('unknown binary expression operator should raise an error', function ()  {
+  it('unsupported binary operators raise an interpreter error', function ()  {
     assert.throws(
-      function () { jsonjs.eval(parse_expr('99 % 2')) }, jsonjs.InterpreterError)
+      function () { jsonjs.eval(parseExpr('99 % 2')) },
+      jsonjs.InterpreterError)
   })
 
 })
